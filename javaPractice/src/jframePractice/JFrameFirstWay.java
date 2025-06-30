bfda1596937eff537483c9747b079bbbebe26f98
c-renz <chlarenzterrones@gmail.com>
2025-06-30 14:46:35 +0800

    created jframefirstway to test one method when tackling jframe

---
+++ javaPractice/src/jframePractice/JFrameFirstWay.java
@@ -0,0 +1,14 @@
+package jframePractice;
+
+import javax.swing.JFrame;
+
+public class JFrameFirstWay extends JFrame {
+	public JFrameFirstWay() {
+		initialize();
+	}
+
+	public void initialize() {
+
+	}
+
+}
