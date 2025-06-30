b6899213bea8ae0e1efb430bce92c7a9642645c1
c-renz <chlarenzterrones@gmail.com>
2025-06-30 19:48:21 +0800

    done with flow layout practice

--- javaPractice/src/jframePractice/FlowLayoutPractice.java
+++ javaPractice/src/jframePractice/FlowLayoutPractice.java
@@ -1,9 +1,16 @@
 package jframePractice;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FlowLayoutPractice {
 	private JFrame frame;
+	private JPanel panel;
 
 	public FlowLayoutPractice() {
 		initialize();
@@ -17,6 +24,16 @@ public class FlowLayoutPractice {
 		this.frame.setSize(800, 500);
 		this.frame.setLocationRelativeTo(null);
 //		this.frame.setResizable(false);
+
+		panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
+		panel.setBackground(Color.red);
+
+		for (int i = 1; i <= 5; i++) {
+			JButton button = new JButton("Button " + Integer.toString(i));
+			panel.add(button);
+		}
+		// add
+		this.frame.add(panel, BorderLayout.CENTER);
 	}
 
 	public void show() {
