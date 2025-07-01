2b784a7c90d2aa98172389af945c6e7a39c00afa
c-renz <chlarenzterrones@gmail.com>
2025-07-01 15:52:35 +0800

    button practice progress

--- javaPractice/src/jframePractice/JButtonPractice.java
+++ javaPractice/src/jframePractice/JButtonPractice.java
@@ -1,11 +1,15 @@
 package jframePractice;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class JButtonPractice {
 	private JFrame frame;
 	private JPanel panel;
+	private JButton button;
 
 	public JButtonPractice() {
 		initialize();
@@ -13,11 +17,23 @@ public class JButtonPractice {
 
 	private void initialize() {
 		frame = new JFrame();
-		this.frame.setTitle("Grid Layout Practice");
+		this.frame.setTitle("JButton Practice");
 		this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.frame.setSize(800, 500);
 		this.frame.setLocationRelativeTo(null);
 //			this.frame.setResizable(false);
+		panel = new JPanel();
+
+		button = createButton();
+		// add
+		this.panel.add(button);
+		this.frame.add(panel, BorderLayout.CENTER);
+	}
+
+	private JButton createButton() {
+		JButton button = new JButton("Print");
+		button.setFocusable(false);
+		return button;
 	}
 
 	public void show() {
